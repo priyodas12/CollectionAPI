040501c6ed8f3420cffc379a0b5370b5fe5f75fe
Priyobrato Das <priyodas12@gmail.com>
2020-04-26 18:32:51 +0530

    key point class added

--- CollectionAPI/src/map/WeakHashMap/WeakHashMapImpl.java
+++ CollectionAPI/src/map/WeakHashMap/WeakHashMapImpl.java
@@ -26,28 +26,3 @@ public class WeakHashMapImpl {
 
 }
 
-class KeyPoint{
-	
-	private String key;
-	
-	public String getKey() {
-		return key;
-	}
-	public void setKey(String key) {
-		this.key = key;
-	}
-	public KeyPoint(String key) {
-		//System.out.println("Key"+key);
-		this.key=key;
-	}
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return "key"+this.key;
-	}
-	@Override
-	protected void finalize() throws Throwable {
-		// TODO Auto-generated method stub
-		super.finalize();
-	}
-}
